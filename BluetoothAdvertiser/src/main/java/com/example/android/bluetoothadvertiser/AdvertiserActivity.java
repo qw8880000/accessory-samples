5f35d288e0dd437f2e57de5dbad8b8c46b08dbc9
qw8880000 <qw8880000@126.com>
2017-04-01 13:58:51 +0800

    refactor BluetoothAdvertiser

--- BluetoothAdvertiser/src/main/java/com/example/android/bluetoothadvertiser/AdvertiserActivity.java
+++ BluetoothAdvertiser/src/main/java/com/example/android/bluetoothadvertiser/AdvertiserActivity.java
@@ -2,16 +2,11 @@ package com.example.android.bluetoothadvertiser;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.AdvertiseCallback;
 import android.bluetooth.le.AdvertiseData;
 import android.bluetooth.le.AdvertiseSettings;
 import android.bluetooth.le.BluetoothLeAdvertiser;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.ParcelUuid;
@@ -27,8 +22,10 @@ import android.widget.Toast;
  * Date: 11/12/14
  * AdvertiserActivity
  */
-public class AdvertiserActivity extends Activity implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
-    private static final String TAG = "AdvertiseActivity";
+public class AdvertiserActivity extends Activity implements
+        SeekBar.OnSeekBarChangeListener, View.OnClickListener, DvpBluetooth.Listener {
+
+    private static final String TAG = AdvertiserActivity.class.getSimpleName();
     private static final int DEFAULT_VALUE = 20;
 
     /* Full Bluetooth UUID that defines the Health Thermometer Service */
@@ -36,6 +33,7 @@ public class AdvertiserActivity extends Activity implements SeekBar.OnSeekBarCha
 
     private BluetoothAdapter mBluetoothAdapter;
     private BluetoothLeAdvertiser mBluetoothLeAdvertiser;
+    private DvpBluetooth mDvpBluetooth;
 
     /* UI to control advertise value */
     private TextView mCurrentValue;
@@ -56,21 +54,14 @@ public class AdvertiserActivity extends Activity implements SeekBar.OnSeekBarCha
         Button button = (Button) findViewById(R.id.update);
         button.setOnClickListener(this);
 
-        /*
-         * Bluetooth in Android 4.3+ is accessed via the BluetoothManager, rather than
-         * the old static BluetoothAdapter.getInstance()
-         */
-        BluetoothManager manager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
-        mBluetoothAdapter = manager.getAdapter();
-       // mBluetoothLeAdvertiser = mBluetoothAdapter.getBluetoothLeAdvertiser();
-
-        openBluetooth();
+        mDvpBluetooth = new DvpBluetooth(this, this);
+        mDvpBluetooth.init();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        closeBluetooth();
+        mDvpBluetooth.deinit();
     }
 
     @Override
@@ -167,56 +158,34 @@ public class AdvertiserActivity extends Activity implements SeekBar.OnSeekBarCha
         }
     }
 
-    private void openBluetooth() {
-        if (!mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivity(enableBtIntent);
-            //mBluetoothAdapter.enable();
+    @Override
+    public void onBluetoothTurnedOn() {
+
+        BluetoothManager manager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        mBluetoothAdapter = manager.getAdapter();
+
+        /*
+        * Check for Bluetooth LE Support.
+        */
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "No LE Support.", Toast.LENGTH_SHORT).show();
+            finish();
         }
 
-        // Register for broadcasts when Bluetooth turned on.
-        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-        registerReceiver(mReceiver, filter);
+        /*
+         * Check for advertising support. Not all devices are enabled to advertise
+         * Bluetooth LE data.
+         */
+        if (!mBluetoothAdapter.isMultipleAdvertisementSupported()) {
+            Toast.makeText(this, "No Advertising Support.", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        /*
+         * we should invoke getBluetoothLeAdvertiser after bluetooth turned on.
+         */
+        mBluetoothLeAdvertiser = mBluetoothAdapter.getBluetoothLeAdvertiser();
+        
+        startAdvertising();
     }
-    private void closeBluetooth() {
-        if (mBluetoothAdapter.isEnabled()) {
-            mBluetoothAdapter.disable();
-        }
-
-        unregisterReceiver(mReceiver);
-    }
-
-    // Create a BroadcastReceiver for Bluetooth turned on.
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)) {
-                int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1);
-                int prevState = intent.getIntExtra(BluetoothAdapter.EXTRA_PREVIOUS_STATE, -1);
-                //Log.d(TAG, "state="+state+" prestate="+prevState);
-                if(prevState == BluetoothAdapter.STATE_TURNING_ON && state == BluetoothAdapter.STATE_ON) {
-
-                    /*
-                     * Check for Bluetooth LE Support.
-                     */
-                    if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-                        Toast.makeText(context, "No LE Support.", Toast.LENGTH_SHORT).show();
-                        finish();
-                    }
-
-                    /*
-                     * Check for advertising support. Not all devices are enabled to advertise
-                     * Bluetooth LE data.
-                     */
-                    if (!mBluetoothAdapter.isMultipleAdvertisementSupported()) {
-                        Toast.makeText(context, "No Advertising Support.", Toast.LENGTH_SHORT).show();
-                        finish();
-                    }
-
-                    mBluetoothLeAdvertiser = mBluetoothAdapter.getBluetoothLeAdvertiser();
-                    startAdvertising();
-                }
-            }
-        }
-    };
 }
