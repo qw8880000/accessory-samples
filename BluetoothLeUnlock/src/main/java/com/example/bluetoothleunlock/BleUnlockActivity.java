f3221fbfd94e56e2a2dc1d3c77dcae15306264a8
qw8880000 <qw8880000@126.com>
2017-04-07 09:37:23 +0800

    Add module BluetoothLeUnlock

---
+++ BluetoothLeUnlock/src/main/java/com/example/bluetoothleunlock/BleUnlockActivity.java
@@ -0,0 +1,13 @@
+package com.example.bluetoothleunlock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BleUnlockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ble_unlock);
+    }
+}
